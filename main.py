15a2b9e57315de29232ab9beb81fd105adca54fc
1hmdsdr <hamid90sadr27@gmail.com>
2026-06-26 14:38:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
+import os
 import requests
 
-SYMBOL = "BTCUSDT"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-url = f"https://api.binance.com/api/v3/ticker/price?symbol={SYMBOL}"
+SYMBOL = "BTCUSDT"
+TARGET_PRICE = 1  # فقط برای تست
+
+# دریافت قیمت از Bybit
+url = f"https://api.bybit.com/v5/market/tickers?category=spot&symbol={SYMBOL}"
 
 response = requests.get(url)
+data = response.json()
 
-print(response.text)
+print(data)
+
+current_price = float(data["result"]["list"][0]["lastPrice"])
+
+print(f"Current price: {current_price}")
+
+if current_price >= TARGET_PRICE:
+
+    message = (
+        f"🚨 ALERT!\n\n"
+        f"{SYMBOL}\n"
+        f"Current Price: {current_price}\n"
+        f"Exchange: Bybit"
+    )
+
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(telegram_url, data=payload)
+
+    print("Alert sent.")
+
+else:
+    print("Target not reached.")
